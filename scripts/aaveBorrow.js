864c691580be57374f94bfb4858012b088446d3e
Arbestor Dinggleback <toms@arbestor.xyz>
2023-01-26 17:00:42 +0800

    DAI borrowing logic added

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -20,6 +20,75 @@ async function main() {
     console.log("Depositing...")
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited!")
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+        lendingPool,
+        deployer
+    )
+    // Find out ETH<->DAI conversion rate before borrowing, by using Chainlink Price Feeds
+    const daiPrice = await getDAIPrice()
+    const amountDaiToBorrow =
+        availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+    console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+    const amountDaiToBorrowWei = ethers.utils.parseEther(
+        amountDaiToBorrow.toString()
+    )
+
+    await borrowDai(
+        networkConfig[network.config.chainId].daiToken,
+        lendingPool,
+        amountDaiToBorrowWei,
+        deployer
+    )
+    await getBorrowUserData(lendingPool, deployer)
+}
+
+async function borrowDai(
+    daiAddress,
+    lendingPool,
+    amountDaiToBorrowWei,
+    account
+) {
+    const borrowTx = await lendingPool.borrow(
+        daiAddress,
+        amountDaiToBorrowWei,
+        1,
+        0,
+        account
+    )
+    await borrowTx.wait(1)
+    console.log("Borrowed!")
+}
+
+async function getDAIPrice() {
+    const daiEthPriceFeed = await ethers.getContractAt(
+        "AggregatorV3Interface",
+        networkConfig[network.config.chainId].daiEthPriceFeed
+    )
+    const price = (await daiEthPriceFeed.latestRoundData())[1]
+    console.log(`DAI/ETH price is ${price.toString()} DAI`)
+    return price
+}
+
+async function getBorrowUserData(lendingPool, account) {
+    const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+        await lendingPool.getUserAccountData(account)
+
+    console.log(
+        `You have ${ethers.utils.formatEther(
+            totalCollateralETH
+        )} worth of ETH deposited`
+    )
+    console.log(
+        `You have ${ethers.utils.formatEther(
+            totalDebtETH
+        )} worth of ETH borrowed`
+    )
+    console.log(
+        `You can borrow ${ethers.utils.formatEther(
+            availableBorrowsETH
+        )} worth of ETH`
+    )
+    return { availableBorrowsETH, totalDebtETH }
 }
 
 async function approveErc20(
